8307537443cb6b8c14ca78694184d2b539d106e7
Tina Gao <thgao@uwaterloo.ca>
2019-05-12 17:09:16 -0400

    added finalised/desired images to folders

--- src/js/digital.js
+++ src/js/digital.js
@@ -1,31 +1,26 @@
 $('document').ready(function (){
 	var imageDiv = getItems();
-	// document.getElementById('photography-grid').innerHTML = imageDiv;
 	var $container = $('#digital-grid');
 	var $items = $(imageDiv);
 	$container.append($items);
-	// var $grid = $('#grid').imagesLoaded( function() {
-		$container.masonry({
-		  itemSelector: '.grid-item',
-		  // columnWidth: 400,
-		  percentPosition: true
-		});
+	$container.masonry({
+	  itemSelector: '.grid-item',
+	  // columnWidth: 400,
+	  percentPosition: true
+	});
 
-	    $items.imagesLoaded().progress(function(imgLoad, image) {
-	      	$container.masonry('layout', $items);
-	    });
-	// });
+    $items.imagesLoaded().progress(function(imgLoad, image) {
+      	$container.masonry('layout', $items);
+    });
 });
 
 
-var $images = ['1.jpg', '2.jpg', '3.jpg','4.jpg','5.jpg','6.jpg', '7.jpg', '8.jpg', '9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg', '15.jpg'];
-// Images Array
 function getItem(image) {
 	return '<div class="grid-item">' + '<img src="src/assets/digital/' + image + '"/></div>\n';
 }
 
 function getItems() {
-	var images = ['1.jpg', '2.jpg', '3.jpg','4.jpg','5.jpg','6.jpg', '7.jpg', '8.jpg', '9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg', '15.jpg'];
+	var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
 	var imageDiv = "";
 	for (var i = 0; i < images.length; i++){
 		imageDiv += getItem(images[i]);
--- src/js/traditional.js
+++ src/js/traditional.js
@@ -15,14 +15,12 @@ $('document').ready(function (){
 });
 
 
-// var $images = ['1.jpg', '2.jpg', '3.jpg','4.jpg','5.jpg','6.jpg', '7.jpg', '8.jpg', '9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg', '15.jpg'];
-// Images Array
 function getItem(image) {
 	return '<div class="grid-item">' + '<img src="src/assets/traditional/' + image + '"/></div>\n';
 }
 
 function getItems() {
-	var images = ['1.jpg', '2.jpg', '3.jpg','4.jpg','5.jpg','6.jpg', '7.jpg', '8.jpg', '9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg', '15.jpg'];
+	var images = ['1.jpg', '2.jpg', '3.jpg','4.jpg','5.jpg','6.jpg', '7.jpg', '8.jpg', '9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg'];
 	var imageDiv = "";
 	for (var i = 0; i < images.length; i++){
 		imageDiv += getItem(images[i]);
