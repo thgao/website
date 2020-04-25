dade42f8bedcf7b511e32e667ee7b0c131f95d38
Tina Gao <thgao@uwaterloo.ca>
2020-04-25 03:22:54 -0400

    remove stale img from js

--- src/js/traditional.js
+++ src/js/traditional.js
@@ -20,7 +20,7 @@ function getItem(image) {
 }
 
 function getItems() {
-	var images = ['1.jpg', '2.jpg', '3.jpg','4.jpg','5.jpg','6.jpg', '7.jpg', '8.jpg', '9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg'];
+	var images = ['1.jpg', '2.jpg', '3.jpg','4.jpg','5.jpg','6.jpg', '7.jpg', '8.jpg', '9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg', '15.jpg'];
 	var imageDiv = "";
 	for (var i = 0; i < images.length; i++){
 		imageDiv += getItem(images[i]);
